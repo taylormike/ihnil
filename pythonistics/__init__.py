e513d0f2de927b27899cd71cc0b82c3afb7d3f98
forstmeier <john.forstmeier@gmail.com>
2015-12-27 21:52:31 -0500

    Update developing reader file code

---
+++ pythonistics/__init__.py
@@ -0,0 +1,3 @@
+"""This is the docstring for the Pythonistics command line tool."""
+
+# https://www.python.org/dev/peps/pep-0257/
--- pythonistics/reader.py
+++ pythonistics/reader.py
@@ -1,35 +1,37 @@
 import argparse
-import io
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("square",
-                    type=int,
-                    help="display a square of a given number")
-parser.add_argument("-v", "--verbosity",
-                    action="count",
-                    default=0,
-                    help="increase output verbosity")
+parser = argparse.ArgumentParser(prog="Pythonistics",
+                                 description="File processer",
+                                 epilog="This is the end")
+
+parser.add_argument("file_name",
+                    type=str,
+                    help="Temporary FILE help string")
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-s", "--short",
+                   action="store_true",
+                   help="Temporary SHORT help string")
+group.add_argument("-l", "--long",
+                   action="store_true",
+                   help="Temporary LONG help string")
+
 args = parser.parse_args()
-answer = args.square**2
 
-if args.verbosity >= 2:
-    print("the square of {} equals {}".format(args.square, answer))
-elif args.verbosity >= 1:
-    print("{}^2 == {}".format(args.square, answer))
+print()
+
+if args.short:
+    print(str(args.file_name) + " is the SHORT version")
+elif args.long:
+    print(str(args.file_name) + " is the LONG version")
 else:
-    print(answer)
+    print(str(args.file_name) + " is the PLAIN version")
 
+with open(args.file_name, 'r') as fn:
+    print()
+    print(fn.name)
+    print()
+    print(fn.read())
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("file_name",
-#                     type=io.IOBase,
-#                     help="Temporary file error string")
-# parser.add_argument("--short",
-#                     help="Temporary short error")
-# parser.add_argument("--long",
-#                     help="Temporary long error")
-# 
-# args = parser.parse_args()
-# 
-# print(args.file_name)
+# https://docs.python.org/3/library/io.html
