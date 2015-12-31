928533de1a74e7747310e7d633e63926ef232f70
forstmeier <john.forstmeier@gmail.com>
2015-12-30 19:54:59 -0500

    Build temporary test file and add it to the ignore file

--- pythonistics/__init__.py
+++ pythonistics/__init__.py
@@ -1,7 +1,4 @@
 """This is the docstring for the Pythonistics command line tool."""
 
 # https://www.python.org/dev/peps/pep-0257/
-
-# JOHN
-# JOHN
-# TOM
+# https://docs.python.org/3/library/io.html
