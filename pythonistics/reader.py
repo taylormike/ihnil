68c124dd3f681b85634c677ec1cedeed21ce63d5
forstmeier <john.forstmeier@gmail.com>
2016-01-05 21:45:55 -0500

    Switch back arguments to one mutually exclusive group

--- pythonistics/reader.py
+++ pythonistics/reader.py
@@ -10,30 +10,17 @@ parser = argparse.ArgumentParser(description="File processer",
 parser.add_argument("file_name",
                     type=argparse.FileType(),
                     help="Temporary %(prog)s POSITIONAL help string")
-parser.add_argument("-e", "--exact",
-                    action="store_true",
-                    help="Temporary EXACT help string")
 
-# Command line operations for what the program should output
-process = parser.add_mutually_exclusive_group()
-process.add_argument("--stats",
-                     action="store_true",
-                     help="Temporary STATS help string")
-process.add_argument("--short",
-                     action="store_true",
-                     help="Temporary SHORT help string")
-process.add_argument("--nicer",
-                     action="store_true",
-                     help="Temporary NICER help string")
-
-# Command line operations for where the results should display
 output = parser.add_mutually_exclusive_group()
-output.add_argument("--toprint",
+output.add_argument("--stats",
+                    action="store_true",
+                    help="Temporary STATS help string")
+output.add_argument("--short",
                     action="store_true",
                     help="Temporary SHORT help string")
-output.add_argument("--tofile",
+output.add_argument("--nicer",
                     action="store_true",
-                    help="Temporary LONG help string")
+                    help="Temporary NICER help string")
 
 args = parser.parse_args()
 string_name = str(args.file_name.name)
@@ -42,16 +29,14 @@ file_extension = os.path.splitext(string_name)[1]
 print()
 
 if file_extension == ".py":
-    if args.toprint:
-        print("{} is the PRINT version\n".format(string_name))
-    elif args.tofile:
-        print("{} is the FILE version\n".format(string_name))
+    if args.stats:
+        print("{} is the STATS version\n".format(string_name))
+    elif args.short:
+        print("{} is the SHORT version\n".format(string_name))
+    elif args.nicer:
+        print("{} is the NICER version\n".format(string_name))
     else:
         print("{} is the OTHER version\n".format(string_name))
-
-    file_contents = args.file_name.read()
-    def_func = str(file_contents.count("def"))
-    class_func = str(file_contents.count("class"))
 
     print()
 
@@ -59,9 +44,7 @@ if file_extension == ".py":
         tokens = list(tokenize.tokenize(t_file.readline))
 
     for token in tokens:
-        token_type = token.type
-        if args.exact:
-            token_type = token.exact_type
+        token_type = token.exact_type
         token_name = tokenize.tok_name[token_type]
         token_range = "{}-{}".format(token.start, token.end)
         print("{: <20}{: <15}{: <15}".format(token_range,
