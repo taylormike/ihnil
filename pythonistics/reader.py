7416cfe6aecd658706bdbece1b1101f59779870f
forstmeier <john.forstmeier@gmail.com>
2016-01-16 23:39:33 -0500

    Establish separate loops for token breakdown

--- pythonistics/reader.py
+++ pythonistics/reader.py
@@ -28,50 +28,38 @@ if file_extension == ".py":
     with tokenize._builtin_open(args.file_name.name, "rb") as t_file:
         tokens = list(tokenize.tokenize(t_file.readline))
 
-    def tokener():
-        for token in tokens:
-            token_name = tokenize.tok_name[token.exact_type]
-            token_range = "{}-{}".format(token.start, token.end)
-            print("{: <20}{: <15}{: <15}".format(token_range,
-                                                 token_name,
-                                                 token.string))
-
-    next_position = 0
-    temporary = ()
+    if_list = []
     for token in tokens:
         if token.string == "if":
-            print_length = len(token.line)
-            if token.start[0] - next_position == 1:
-                print(" * " * print_length)
-                print("Nested 'if' statements")
-                print(temporary[0], temporary[1].rstrip())
-                print(token.start, token.line.rstrip())
-                print(" * " * print_length)
-            next_position = token.start[0]
-            temporary = (token.start, token.line)
-    # Multiple nested if loops -> list comprehension
-    #   Generate different results based on
-    #   what conditionals are used (e.g. "!=" or "==")
+            if_line = token.line.rstrip()
+            if_row = token.start[0]
+            if_list.append([if_row, if_line])
 
-    # TODO (1/14/16) - add in the token collection list
+    b_count = 0
+    for if_value in if_list:
+        a_count = if_value[0]
+        a_value = str(if_value)
+        if a_count - b_count == 1:
+            print(b_value)
+            print(a_value)
+        b_count = a_count
+        b_value = a_value
 
-    for token in tokens:
-        if token.string == "if":
-            position = token.start
-            if position[1] - next_position == 0:
-                print("Stacked 'if' statements")
-            next_position = position[1]
+
+
+    # --------------------------------------------------------------
     # Stacked if conditional loops -> dict lookup
     #   Generate possible dict structures
-
+    # --------------------------------------------------------------
     # Identical code -> functions
     # Identical string -> string variables
     # Triple quotes on three lines -> single line docstring
-
+    # --------------------------------------------------------------
     # Printing out comment lines to the script will require
     #   the file to be opened with 'read' and 'write' permissions
     # Also include code necessary for identifying the
     #   printed comments
+    # --------------------------------------------------------------
 
     if args.read:
         print("{} is the READ version".format(string_name))
