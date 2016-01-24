41a064648a25973222ced6998dfc4d87cd6270bf
forstmeier <john.forstmeier@gmail.com>
2016-01-23 23:14:55 -0500

    Build three list comprehension tools

--- pythonistics/reader.py
+++ pythonistics/reader.py
@@ -28,38 +28,21 @@ if file_extension == ".py":
     with tokenize._builtin_open(args.file_name.name, "rb") as t_file:
         tokens = list(tokenize.tokenize(t_file.readline))
 
-    line_list = [(token.start, token.line.rstrip())
-                 for token in tokens
-                 if token.string == "\n"]
+    rows = list(set([token.start[0] for token in tokens]))
+    row_list = [token.line.rstrip() for token in tokens
+                if token.string == "\n"]
+    token_list = [[(token.string, tokenize.tok_name[token.exact_type])
+                  for token in tokens
+                  if token.start[0] == row_var]
+                  for row_var in rows]
 
-    row = 0
-    row_list, token_list = [], []
-    for token in tokens:
-        values = (token.string, tokenize.tok_name[token.exact_type])
-        if token.start[0] == row:
-            row_list.append(values)
-        else:
-            token_list.append([row, row_list])
-            row_list = []
-            row_list.append(values)
-            row += 1
+    if_list = [i[0] for i in token_list if "if" in [j[0] for j in i[2]]]
 
-    # --------------------------------------------------------------
-    # Stacked if conditional loops -> dict lookup
-    #   Generate possible dict structures
-    # --------------------------------------------------------------
-    # Identical code -> functions
-    # Identical string -> string variables
-    # Triple quotes on three lines -> single line docstring
     # --------------------------------------------------------------
     # Printing out comment lines to the script will require
     #   the file to be opened with 'read' and 'write' permissions
     # Also include code necessary for identifying the
     #   printed comments
-    # --------------------------------------------------------------
-    # Apply pep8 to each document that is run through pythonistics
-    #   This will keep all code looking "pythonic" for analysis
-    # --------------------------------------------------------------
 
     if args.read:
         print("{} is the READ version".format(string_name))
