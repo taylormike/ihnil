f287a52d392ac0487740b5d83e7c41ef7c90ea8b
forstmeier <john.forstmeier@gmail.com>
2016-01-26 19:56:50 -0500

    Initialize full nested if row value finder algorithm

--- pythonistics/reader.py
+++ pythonistics/reader.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import tokenize
+import operator
 import itertools
 
 
@@ -29,20 +30,14 @@ if file_extension == ".py":
     with tokenize._builtin_open(args.file_name.name, "rb") as t_file:
         tokens = list(tokenize.tokenize(t_file.readline))
 
-    rows = list(set([token.start[0] for token in tokens]))
-    row_list = [token.line.rstrip() for token in tokens
-                if token.string == "\n"]
-    row_list.insert(0, "\n")
-    row_list.append("\n")
-    token_list = [[(token.string, tokenize.tok_name[token.exact_type])
-                  for token in tokens
-                  if token.start[0] == row_var]
-                  for row_var in rows]
-
-    combo_list = [[i, j, k] for i, j, k in zip(rows, row_list, token_list)]
-
     full_list = [(g, list(i))
-                 for g, i in itertools.groupby(token, lambda x: x.start[0])]
+                 for g, i in itertools.groupby(tokens, lambda x: x.start[0])]
+
+    if_rows = [i[0] for i in full_list if "if" in [j.string for j in i[1]]]
+    
+    if_nest = [list(map(operator.itemgetter(1), i))
+               for g, i in itertools.groupby(enumerate(if_rows),
+               lambda ix: ix[0] - ix[1])]
 
     # --------------------------------------------------------------
     # Printing out comment lines to the script will require
@@ -53,10 +48,8 @@ if file_extension == ".py":
 
     if args.read:
         print("{} is the READ version".format(string_name))
-
     elif args.write:
         print("{} is the WRITE version".format(string_name))
-
     else:
         print("{} is the OTHER version".format(string_name))
 
