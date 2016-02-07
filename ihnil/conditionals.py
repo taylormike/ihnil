db87370b8abf2c4e9caab7c09991467a23bb560a
forstmeier <john.forstmeier@gmail.com>
2016-02-06 19:51:52 -0500

    Remove as-of-yet unnecessary file

--- ihnil/conditionals.py
+++
@@ -1,8 +0,0 @@
-class EvaluationIHNIL(object):
-    def __init__ = (self, if_tokens, excl_names, cond_names):
-        self.if_tokens = if_tokens
-        self.exclude = exclude
-        self.include = include
-
-def evaluation_func(toks, excl, incl):
-    pass
