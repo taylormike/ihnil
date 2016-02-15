75498f5fee50579fb199d007f811a93360391035
forstmeier <john.forstmeier@gmail.com>
2016-02-15 15:33:42 -0500

    Restarting with bare minimum argument parsing structure

--- ihnil/reader.py
+++ ihnil/reader.py
@@ -18,7 +18,6 @@ Arguments:
 import argparse
 import os
 import ast
-import codegen
 
 
 parser = argparse.ArgumentParser(description="Python 'if' loop optimizer",
@@ -43,47 +42,7 @@ file_extension = os.path.splitext(string_name)[1]
 
 
 class MainIHNIL(object):
-    def __init__(self, inpt):
-        self.inpt = inpt
-        self.module = ast.parse(self.inpt)
-
-    def selector(self, num):
-        self.num = num
-
-    def _first1(self):
-        print(codegen.to_source(node))
-
-    def _first2(self):
-        print(node._fields)
-
-    def _first3(self):
-        print("Nested error on line {}".format(node.lineno))
-
-    def _second1(self):
-        for itm in node.body:
-            if isinstance(itm, ast.If):
-                print("Start line {}".format(itm.lineno))
-                print(codegen.to_source(itm) + "\n")
-
-    def _second2(self):
-        for itm in node.body:
-            if isinstance(itm, ast.If):
-                print(itm._fields)
-
-    def _second3(self):
-        for itm in node.body:
-            if isinstance(itm, ast.If):
-                print("Nested error on line {}".format(itm.lineno))
-
-    FIRST = {1: _first1, 2: _first2, 3: _first3}
-    SECOND = {1: _second1, 2: _second2, 3: _second3}
-
-    def result_out(self):
-        for node in self.module.body:
-            if isinstance(node, ast.If):
-                self.FIRST[self.num](self)
-            elif isinstance(node, (ast.FunctionDef, ast.ClassDef)):
-                self.SECOND[self.num](self)
+    pass
 
 if file_extension == ".py":
     with open(args.file_name.name) as f:
@@ -92,15 +51,9 @@ if file_extension == ".py":
 
     if args.read:
         print("READ")
-        instance.selector(1)
-        instance.result_out()
     elif args.write:
         print("WRITE")
-        instance.selector(2)
-        instance.result_out()
     else:
         print("ELSE")
-        instance.selector(3)
-        instance.result_out()
 else:
     print("\nPlease enter a Python file\n")
