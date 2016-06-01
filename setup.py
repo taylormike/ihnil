fbc7a8c2aff21914bc78792473753d4bf3a5c106
forstmeier <john.forstmeier@gmail.com>
2016-06-01 19:23:02 -0400

    Repair TravisCI file and setup documents

--- setup.py
+++ setup.py
@@ -2,23 +2,13 @@ import re
 import setuptools
 
 
-# version = re.search('^__version__\s*=\s*"(.*)"',
-#                     open("ihnil/ihnil.py").read(),
-#                     re.M).group(1)
-
-# with open("README.md", "r") as f:
-#     long_description = f.read()
-
 setuptools.setup(name="ihnil",
-                #  version=version,
                  version="1.0.0",
                  description="Python 'if' loop optimizer",
-                #  long_description=long_description,
                  url="https://github.com/forstmeier/ihnil",
                  license="MIT",
                  author="John Forstmeier",
-                #  author_email="john.forstmeier@gmail.com",
                  packages=setuptools.find_packages(),
-                 install_requires=["argparse", "os", "ast", "codegen"],
+                #  install_requires=["argparse", "os", "ast", "codegen"],
                  entry_points={
-                    "console_scripts": ["ihnil = ihnil.ihnil.py"]},)
+                    "console_scripts": ["ihnil = ihnil.ihnil:main"]},)
